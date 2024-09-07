ea5a48f91fea0de5016e74485e5418caaf3a434f
Ben Healey <bentastic27@gmail.com>
2024-09-07 18:45:53 -0400

    change-apply seems to be working

--- change-apply/app.py
+++ change-apply/app.py
@@ -1,20 +1,40 @@
-from flask import Flask, request
+import json
 
+import boto3
+from flask import Flask, request
 from cloudevents.http import from_http
 
+
 app = Flask(__name__)
+r53client = boto3.client('route53')
 
 
 @app.route("/", methods=["POST"])
 def home():
-  event = from_http(request.headers, request.get_data())
+  try:
+    event = from_http(request.headers, request.get_data()).get_data()
 
-  print(
-    f"Found {event['id']} from {event['source']} with type "
-    f"{event['type']} and specversion {event['specversion']}"
-  )
+    r53client.change_resource_record_sets(
+      HostedZoneId=event["hosted_zone_id"],
+      ChangeBatch={
+        "Changes": [{
+          "Action": "UPSERT",
+          "ResourceRecordSet": {
+            "Name": event["record_name"],
+            "Type": event["record_type"],
+            "TTL": int(event["record_ttl"]),
+            "ResourceRecords": [{
+              "Value": event["wan_ip"]
+            }]
+          }
+        }]
+      }
+    )
+    return "", 204
 
-  return "", 204
+  except Exception as e:
+    print(e)
+    return "", 500
 
 
 if __name__ == "__main__":
